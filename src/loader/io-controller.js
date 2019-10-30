30ca4486739b1b8c4f887964bbc938133e0af02c
HUGY1 <hugangyu@live.cn>
2019-10-30 18:01:38 +0800

    test

--- src/loader/io-controller.js
+++ src/loader/io-controller.js
@@ -315,6 +315,7 @@ class IOController {
     }
 
     _dispatchChunks(chunks, byteStart) {
+        console.log(chunks);
         // this._currentRange.to = byteStart + chunks.byteLength - 1;
         return this._onInitChunkArrival(chunks, byteStart);
     }
