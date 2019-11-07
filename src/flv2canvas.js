7fb412092412d0a265b9e745a4521253d8dc9b09
HUGY1 <hugangyu@live.cn>
2019-11-07 16:33:41 +0800

    update

--- src/flv2canvas.js
+++ src/flv2canvas.js
@@ -1,22 +1,36 @@
+/* eslint-disable no-case-declarations */
 
 import flv2canvasLoader from './flv2canvas.loader';
 
 class flv2canvas {
     constructor(options) {
+        this.playWidth = 0;
+        this.playHeight = 0;
+        this.videoBuffer = [];
+        this.videoDts = [];
+        this.startPlay = false;
+        this.options = options;
+
         this.loadCtl = new flv2canvasLoader(options);
         let loader = this.loader = this.loadCtl.createLoader();
         this.initWorker();
+
+       
     }
 
     load() {
         this.loader.load();
     }
+    
+    play() {
+        this.startPlay = true;
+    }
 
     initWorker() {
-        this.worker = new Worker('/dist/flv2canvas.decoder.js');
+        this.worker = new Worker(this.options.workFile);
         this.loadCtl.worker = this.worker;
         this.worker.postMessage({
-            type: "Broadway.js - Worker init", options: {
+            type: 'Broadway.js - Worker init', options: {
                 rgb: false,
                 memsize: '',
                 reuseMemory: true
@@ -25,20 +39,20 @@ class flv2canvas {
 
         let self = this;
         this.worker.addEventListener('message', function (e) {
-            var data = e.data;
+            let data = e.data;
             if (data.consoleLog) {
                 console.log(data.consoleLog);
                 return;
-            };
+            }
 
-            var type = e.data.type;
+            let type = e.data.type;
             switch (type) {
-                case "playAudio":
-                    var typeArr = e.data.buffer;
-                    var sampleRate = e.data.sampleRate;
-                    var dts = e.data.dts;
+                case 'playAudio':
+                    let typeArr = e.data.buffer;
+                    let sampleRate = e.data.sampleRate;
+                    let dts = e.data.dts;
                     if (!window.audioCtrl.sampleRate) {
-                        console.log('sampleRate', sampleRate)
+                        console.log('sampleRate', sampleRate);
 
                         window.audioCtrl.sampleRate = sampleRate;
                     }
@@ -46,22 +60,38 @@ class flv2canvas {
                     window.audioCtrl.feed(typeArr);
                     break;
 
-                case "playVideo":
-                    var typeArr = e.data.buffer;
-                    var width = e.data.width;
-                    var height = e.data.height;
+                case 'playVideo':
+                    let typeArr2 = e.data.buffer;
+                    let width = e.data.width;
+                    let height = e.data.height;
 
-                    console.log(width,height)
-                    // onPictureDecoded.call(self, typeArr, width, height, data.infos);
+                    self.onPictureDecoded(typeArr, width, height, data.infos);
                     break;
 
-                case "initFinished":
-                    self.load()
+                case 'initFinished':
+                    self.load();
                     break;
             }
 
         }, false);
 
+    }
+
+    onPictureDecoded(buffer, width, height, infos) {
+        this.playWidth = width;
+        this.playHeight = height;
+        this.videoBuffer.push(new Uint8Array(buffer));
+        let self = this;
+        if (this.videoBuffer.length > 50 && !this.startPlay) {
+            console.log('clear', this.videoBuffer.length, this.videoDts.length);
+            this.videoBuffer = [];
+            // self.videoDts = []
+            let i = 0;
+            while (i < 50) {
+                self.videoDts.shift();
+                i++;
+            }
+        }
     }
 
 
